5e5a581bbb8c745582c4de132a2a84efaf45b9b1
MOHAMMED ZAHED KHAN <24bk1a66r0@stpetershyd.com>
2025-07-09 21:13:40 +0530

    Create AREA OF RECTANGLE.java

---
+++ AREA OF RECTANGLE.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Rectangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter Length: ");
+        int Length = scanner.nextInt();
+
+        System.out.print("Enter Breath: ");
+        int Breath = scanner.nextInt();
+
+        int area = Length * Breath;
+        System.out.println("Area of the rectangle is: " + area);
+
+        scanner.close();
+    }
+}
