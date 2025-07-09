70fe957d72414500b9a6b5f2a0fb25c3575c0aa4
MOHAMMED ZAHED KHAN <24bk1a66r0@stpetershyd.com>
2025-07-09 16:45:22 +0530

    Create My first JAVA program.java

---
+++ My first JAVA program.java
@@ -0,0 +1,6 @@
+public class Firstprogram {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("My Name Is MOHAMMED ZAHED KHAN");
+    }
+}
