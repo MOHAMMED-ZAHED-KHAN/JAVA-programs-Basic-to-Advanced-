a27d4cbd1db32dd81bd0db7b0c97751ef65931c1
MOHAMMED ZAHED KHAN <24bk1a66r0@stpetershyd.com>
2025-07-10 21:58:35 +0530

    Create ARITHMETIC OPERATORS.java

---
+++ ARITHMETIC OPERATORS.java
@@ -0,0 +1,27 @@
+
+
+public class Arithmeticoperators {
+    public static void main(String[] args) {
+        int x = 6;
+        // int y = x+4;
+        // int y = x-4;
+        // int y = x*4;
+        // float y = x/4f;
+        // double y = x%4;
+        int y = 9;
+        //  y += 3;
+        //  y *= 3;
+        //  y -= 3;
+        //  System.out.println(y);
+        //  System.out.println(6==8);
+        //  System.out.println(6>8);
+        //  System.out.println(6<98);
+        //  System.out.println(6==6);
+        //  System.out.println(6+6+6);
+        System.out.println(65>5 && 68>16);
+        System.out.println(86>2 && 65>98);
+        System.out.println(65>6 || 65>64);
+        System.out.println(65>5 || 65>98);
+
+    }
+}
