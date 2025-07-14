c49e976ba205308be0abb9bfea34802b8d02e0b8
MOHAMMED ZAHED KHAN <24bk1a66r0@stpetershyd.com>
2025-07-14 22:42:38 +0530

    Create 1 TO 10 USING LOOPS.java

---
+++ 1 TO 10 USING LOOPS.java
@@ -0,0 +1,64 @@
+
+import java.util.Scanner;
+
+public class onetoten {
+    public static void main(String[] args) {
+
+        
+        Scanner scanner = new Scanner(System.in);
+
+       
+        /*                                      USING WHILE LOOP
+
+
+        System.out.println("Enter i value: ");
+
+        int i = scanner.nextInt();
+
+        System.out.println("Numbers  from " + i + " to 10 are: ");
+
+        while(i<=10){
+            System.out.println(i);
+            i++;
+        }
+
+        
+        */
+
+        
+
+        /*                                      USING DO-WHILE LOOP
+
+
+        System.out.print("Enter i value: ");
+
+        int i = scanner.nextInt();
+
+        System.out.println("Numbers  from " + i + " to 10 are: ");
+
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<=10);
+
+
+        */
+
+
+        /*                                      USING FOR LOOP
+
+
+        System.out.println("Numbers from 1 to 10 are: ");
+
+        for (int i = 1 ; i<=10 ; i++) {
+            System.out.println(i);
+        }
+
+
+        */
+        
+        scanner.close();
+
+    }
+        
+}
