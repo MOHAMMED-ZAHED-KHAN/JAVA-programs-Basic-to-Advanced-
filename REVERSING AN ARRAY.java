1d4d482b89a48fac43b11c42b218066ef51ee629
MOHAMMED ZAHED KHAN <24bk1a66r0@stpetershyd.com>
2025-08-13 20:22:01 +0530

    Update REVERSING AN ARRAY.java

--- REVERSING AN ARRAY.java
+++ REVERSING AN ARRAY.java
@@ -5,27 +5,39 @@ public class reverseOfArray {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int [] arr = {1,2,3,4,5,6};
-        int l=arr.length;
-        int n = Math.floorDiv(l,2);
-        System.out.println("Elements are: ");
-        for(int i=0;i<6;i++)
-        {
-            System.out.println(arr[i]);
-        }
-        for(int i=0;i<n;i++)
-        {
-            int temp=arr[i];
-            arr[i]=arr[l-1-i];
-            arr[l-1-i]=temp;
-        }
-        System.out.println("Reverse is: ");
-        for(int i=0;i<6;i++)
-        {
-            System.out.println(arr[i]);
-        }
+      System.out.print("Enter the size of the array: ");
+      int n = scanner.nextInt();
+
+      int [] arr = new int[n];
+      System.out.println("Enter the elements of the array: ");
+      for(int i=0;i<n;i++)
+      {
+        arr[i]=scanner.nextInt();
+      }
+
+      System.out.println("The elements of the array are: ");
+      for(int i=0;i<n;i++)
+      {
+        System.out.println(arr[i]);
+      }
+
+      int l = arr.length;
+      int m = Math.floorDiv(l, 2);
+      
+      for(int i=0;i<m;i++)
+      {
+        int temp = arr[i];
+        arr[i]=arr[l-1-i];
+        arr[l-1-i]=temp;
+      }
+      System.out.println("The Reverse of the array is: ");
+      for(int i=0;i<n;i++)
+      {
+        System.out.println(arr[i]);
+      }
 
 
-        scanner.close();
+
+      scanner.close();
     }
 }
